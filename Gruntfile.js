da85a2cf5acc02878ebb10d54cfa59e00a930200
Chris Pearce <crite70@hotmail.com>
2015-06-06 21:13:09 +1000

    Working on Grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,6 @@
+// @credit
+// https://github.com/ozasadnyy/optimized-jekyll-grunt/blob/master/Gruntfile.js
+
 'use strict';
 
 module.exports = function(grunt) {
@@ -10,19 +13,21 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
 
+      // Setup env vars
       app: {
-        app:      'app',
+        source:   'app',
         dist:     'dist',
         baseurl:  ''
       },
 
+      // Start `watch`
       watch: {
 
         // ***************************************************************** //
         // SASS COMPILATION & AUTOPREFIXER
         // ***************************************************************** //
         sass: {
-          files: ['<%= app.app %>/_assets/scss/**/*.{scss,sass}'],
+          files: ['<%= app.source %>/_assets/scss/**/*.{scss,sass}'],
           tasks: ['sass:server', 'autoprefixer']
         },
 
@@ -30,7 +35,7 @@ module.exports = function(grunt) {
         // MINIFY JS
         // ***************************************************************** //
         scripts: {
-          files: ['<%= app.app %>/_assets/js/**/*.{js}'],
+          files: ['<%= app.source %>/_assets/js/**/*.{js}'],
           tasks: ['uglify']
         },
 
@@ -38,7 +43,7 @@ module.exports = function(grunt) {
         // IMAGES
         // ***************************************************************** //
         images: {
-            files: ['<%= app.app %>/img/**/*.{gif,jpg,jpeg,png,svg,webp}'],
+            files: ['<%= app.source %>/img/**/*.{gif,jpg,jpeg,png,svg,webp}'],
             tasks: ['copy:server']
         },
 
@@ -46,7 +51,7 @@ module.exports = function(grunt) {
         // JEKYLL TASKS WITH LIVE-RELOAD
         // ***************************************************************** //
         jekyll: {
-          files: ['<%= app.app %>/**/*.{html,yml,md,mkd,markdown}'],
+          files: ['<%= app.source %>/**/*.{html,yml,md,mkd,markdown}'],
           tasks: ['jekyll:server']
           },
           livereload: {
@@ -80,7 +85,7 @@ module.exports = function(grunt) {
               base: [
                 '.jekyll',
                 '.tmp',
-                '<%= app.app %>'
+                '<%= app.source %>'
               ]
             }
           },
@@ -123,246 +128,244 @@ module.exports = function(grunt) {
         jekyll: {
           options: {
             config: '_config.yml,_config.build.yml',
-            src: '<%= app.app %>'
-            },
-            dist: {
-              options: {
-                dest: '<%= app.dist %>/<%= app.baseurl %>',
-              }
-            },
-            server: {
-              options: {
-                config: '_config.yml',
-                dest: '.jekyll/<%= app.baseurl %>'
-              }
-            }
+            src: '<%= app.source %>'
           },
-
-          // *************************************************************** //
-          // MINIFY HTML
-          // *************************************************************** //
-          htmlmin: {
-            dist: {
-              options: {
-                removeComments: true,
-                collapseWhitespace: true,
-                collapseBooleanAttributes: true,
-                removeAttributeQuotes: true,
-                removeRedundantAttributes: true,
-                removeEmptyAttributes: true,
-                minifyJS: true,
-                minifyCSS: true
-              },
-              files: [{
-                expand: true,
-                cwd: '<%= app.dist %>/<%= app.baseurl %>',
-                src: '**/*.html',
-                dest: '<%= app.dist %>/<%= app.baseurl %>'
-              }]
-            }
-          },
-
-          // *************************************************************** //
-          // MINIFY JS
-          // *************************************************************** //
-          uglify: {
-            server: {
-              options: {
-                mangle: false,
-                beautify: true
-              },
-              files: {
-                '.tmp/<%= app.baseurl %>/js/scripts.js': ['<%= app.app %>/_assets/js/**/*.js']
-              }
-            },
-            dist: {
-              options: {
-                compress: true,
-                preserveComments: false,
-                report: 'min'
-              },
-              files: {
-                '<%= app.dist %>/<%= app.baseurl %>/js/scripts.js': ['<%= app.app %>/_assets/js/**/*.js']
-              }
-            }
-        },
-
-          // *************************************************************** //
-          // SASS COMPILATION
-          // *************************************************************** //
-          sass: {
-            server: {
-              options: {
-                sourceMap: false
-              },
-              files: [{
-                expand: true,
-                cwd: '<%= app.app %>/_assets/scss/',
-                src: '**/*.{scss,sass}',
-                dest: '.tmp/<%= app.baseurl %>/css',
-                ext: '.css'
-              }]
-            },
-            dist: {
-              options: {
-                style: 'compressed',
-                sourceMap: false
-              },
-              files: [{
-                expand: true,
-                cwd: '<%= app.app %>/_assets/scss',
-                src: '**/*.{scss,sass}',
-                dest: '<%= app.dist %>/<%= app.baseurl %>/css',
-                ext: '.css'
-              }]
-            }
-          },
-
-          // *************************************************************** //
-          // REMOVE UNNEEDED CSS
-          // *************************************************************** //
-          uncss: {
+          dist: {
             options: {
-              htmlroot: '<%= app.dist %>/<%= app.baseurl %>',
-              report: 'gzip'
-            },
-            dist: {
-              src: '<%= app.dist %>/<%= app.baseurl %>/**/*.html',
-              dest: '.tmp/<%= app.baseurl %>/css/style.css'
+              dest: '<%= app.dist %>/<%= app.baseurl %>',
             }
           },
-
-          // *************************************************************** //
-          // AUTOPREFIXER
-          // *************************************************************** //
-          autoprefixer: {
+          server: {
             options: {
-              browsers: ['last 2 versions']
-            },
-            server: {
-              files: [{
-                expand: true,
-                cwd: '.tmp/<%= app.baseurl %>/css',
-                src: '**/*.css',
-                dest: '.tmp/<%= app.baseurl %>/css'
-              }]
-            },
-            dist: {
-              files: [{
-                expand: true,
-                cwd: '<%= app.dist %>/<%= app.baseurl %>/css',
-                src: '**/*.css',
-                dest: '<%= app.dist %>/<%= app.baseurl %>/css'
-              }]
-            }
-        },
-
-          // *************************************************************** //
-          // LOAD CRITICAL CSS
-          // *************************************************************** //
-          critical: {
-            dist: {
-              options: {
-                base: './',
-                css: ['.tmp/<%= app.baseurl %>/css/style.css'],
-                minify: true,
-                width: 320,
-                height: 480
-              },
-              files: [{
-                expand: true,
-                cwd: '<%= app.dist %>/<%= app.baseurl %>',
-                src: ['**/*.html'],
-                dest: '<%= app.dist %>/<%= app.baseurl %>'
-              }]
-            }
-          },
-
-          // *************************************************************** //
-          // MINIFY CSS
-          // *************************************************************** //
-          cssmin: {
-            dist: {
-              options: {
-                keepSpecialComments: 0,
-                check: 'gzip'
-              },
-              files: [{
-                expand: true,
-                cwd: '.tmp/<%= app.baseurl %>/css',
-                src: ['*.css'],
-                dest: '.tmp/<%= app.baseurl %>/css'
-              }]
-            }
-          },
-
-          // *************************************************************** //
-          // OPTIMISE IMAGES
-          // *************************************************************** //
-          imagemin: {
-            options: {
-              progressive: true,
-              cache: false,
-              optimizationLevel: 4
-            },
-            dist: {
-              files: [{
-                expand: true,
-                cwd: '<%= app.app %>/img',
-                src: ['**/*.{png,jpg,jpeg,gif}'],
-                dest: '<%= app.dist %>/<%= app.baseurl %>/img'
-              }]
-            }
-          },
-
-          // *************************************************************** //
-          // MINIFY SVG
-          // *************************************************************** //
-          svgmin: {
-            dist: {
-              files: [{
-                expand: true,
-                cwd: '<%= app.app %>/img',
-                src: '**/*.svg',
-                dest: '<%= app.dist %>/<%= app.baseurl %>/img'
-              }]
-            }
-          },
-
-          // *************************************************************** //
-          // COPY FILES AND FOLDERS
-          // *************************************************************** //
-          copy: {
-            dist: {
-              files: [{
-                expand: true,
-                dot: true,
-                cwd: '.tmp/<%= app.baseurl %>',
-                src: [
-                  'css/**/*',
-                  'js/**/*'
-                ],
-                dest: '<%= app.dist %>/<%= app.baseurl %>'
-              }]
-            }
-          },
-
-          // *************************************************************** //
-          // GIT COMMIT AND PUSH TO GH-PAGES
-          // *************************************************************** //
-          buildcontrol: {
-            dist: {
-              options: {
-                dir: '<%= app.dist %>/<%= app.baseurl %>',
-                remote: 'git@github.com:chris-pearce/scally-website.git',
-                branch: 'gh-pages',
-                commit: true,
-                push: true,
-                connectCommits: false
-              }
+              config: '_config.yml',
+              dest: '.jekyll/<%= app.baseurl %>'
             }
           }
-        });
+        },
+
+        // *************************************************************** //
+        // MINIFY HTML
+        // *************************************************************** //
+        htmlmin: {
+          dist: {
+            options: {
+              removeComments: true,
+              collapseWhitespace: true,
+              collapseBooleanAttributes: true,
+              removeAttributeQuotes: true,
+              removeRedundantAttributes: true,
+              removeEmptyAttributes: true,
+              minifyJS: true,
+              minifyCSS: true
+            },
+            files: [{
+              expand: true,
+              cwd: '<%= app.dist %>/<%= app.baseurl %>',
+              src: '**/*.html',
+              dest: '<%= app.dist %>/<%= app.baseurl %>'
+            }]
+          }
+        },
+
+        // *************************************************************** //
+        // MINIFY JS
+        // *************************************************************** //
+        uglify: {
+          server: {
+            options: {
+              mangle: false,
+              beautify: true
+            },
+            files: {
+              '.tmp/<%= app.baseurl %>/js/scripts.js': ['<%= app.source %>/_assets/js/**/*.js']
+            }
+          },
+          dist: {
+            options: {
+              compress: true,
+              preserveComments: false,
+              report: 'min'
+            },
+            files: {
+              '<%= app.dist %>/<%= app.baseurl %>/js/scripts.js': ['<%= app.source %>/_assets/js/**/*.js']
+            }
+          }
+        },
+
+        // *************************************************************** //
+        // SASS COMPILATION
+        // *************************************************************** //
+        sass: {
+          server: {
+            options: {
+              sourceMap: false
+            },
+            files: [{
+              expand: true,
+              cwd: '<%= app.source %>/_assets/scss/',
+              src: '**/*.{scss,sass}',
+              dest: '.tmp/<%= app.baseurl %>/css',
+              ext: '.css'
+            }]
+          },
+          dist: {
+            options: {
+              style: 'compressed',
+              sourceMap: false
+            },
+            files: [{
+              expand: true,
+              cwd: '<%= app.source %>/_assets/scss',
+              src: '**/*.{scss,sass}',
+              dest: '<%= app.dist %>/<%= app.baseurl %>/css',
+              ext: '.css'
+            }]
+          }
+        },
+
+        // *************************************************************** //
+        // REMOVE UNNEEDED CSS
+        // N.B. CURRENTLY NOT WORKING  (ಥ﹏ಥ)
+        // *************************************************************** //
+        uncss: {
+          options: {
+            htmlroot: '<%= app.dist %>/<%= app.baseurl %>',
+            report: 'gzip'
+          },
+          dist: {
+            src: '<%= app.dist %>/<%= app.baseurl %>/**/*.html',
+            dest: '.tmp/<%= app.baseurl %>/css/style.css'
+          }
+        },
+
+        // *************************************************************** //
+        // AUTOPREFIXER
+        // *************************************************************** //
+        autoprefixer: {
+          options: {
+            browsers: ['last 1 version']
+          },
+          server: {
+            files: [{
+              expand: true,
+              cwd: '.tmp/<%= app.baseurl %>/css',
+              src: '**/*.css',
+              dest: '.tmp/<%= app.baseurl %>/css'
+            }]
+          },
+          dist: {
+            files: [{
+              expand: true,
+              cwd: '<%= app.dist %>/<%= app.baseurl %>/css',
+              src: '**/*.css',
+              dest: '<%= app.dist %>/<%= app.baseurl %>/css'
+            }]
+          }
+        },
+
+        // *************************************************************** //
+        // LOAD CRITICAL CSS
+        // *************************************************************** //
+        critical: {
+          dist: {
+            options: {
+              base: './',
+              css: '<%= app.dist %>/<%= app.baseurl %>/css/style.css',
+              width: 1600,
+              height: 900
+            },
+            src:  '<%= app.dist %>/<%= app.baseurl %>/index.html',
+            dest: '<%= app.dist %>/<%= app.baseurl %>/index.html'
+          }
+        },
+
+        // *************************************************************** //
+        // MINIFY CSS
+        // *************************************************************** //
+        cssmin: {
+          dist: {
+            options: {
+              keepSpecialComments: 0,
+              check: 'gzip'
+            },
+            files: [{
+              expand: true,
+              cwd: '.tmp/<%= app.baseurl %>/css',
+              src: ['*.css'],
+              dest: '.tmp/<%= app.baseurl %>/css'
+            }]
+          }
+        },
+
+        // *************************************************************** //
+        // OPTIMISE IMAGES
+        // *************************************************************** //
+        imagemin: {
+          options: {
+            progressive: true,
+            cache: false,
+            optimizationLevel: 3
+          },
+          dist: {
+            files: [{
+              expand: true,
+              cwd: '<%= app.source %>/img',
+              src: ['**/*.{png,jpg,jpeg,gif}'],
+              dest: '<%= app.dist %>/<%= app.baseurl %>/img'
+            }]
+          }
+        },
+
+        // *************************************************************** //
+        // MINIFY SVG
+        // *************************************************************** //
+        svgmin: {
+          dist: {
+            files: [{
+              expand: true,
+              cwd: '<%= app.source %>/img',
+              src: '**/*.svg',
+              dest: '<%= app.dist %>/<%= app.baseurl %>/img'
+            }]
+          }
+        },
+
+        // *************************************************************** //
+        // COPY FILES AND FOLDERS
+        // *************************************************************** //
+        copy: {
+          dist: {
+            files: [{
+              expand: true,
+              dot: true,
+              cwd: '.tmp/<%= app.baseurl %>',
+              src: [
+                'css/**/*',
+                'js/**/*'
+              ],
+              dest: '<%= app.dist %>/<%= app.baseurl %>'
+            }]
+          }
+        },
+
+        // *************************************************************** //
+        // GIT COMMIT AND PUSH TO GH-PAGES
+        // *************************************************************** //
+        buildcontrol: {
+          dist: {
+            options: {
+              dir: '<%= app.dist %>/<%= app.baseurl %>',
+              remote: 'git@github.com:chris-pearce/scally-website.git',
+              branch: 'gh-pages',
+              commit: true,
+              push: true,
+              connectCommits: false
+            }
+          }
+        }
+
+      // End `watch`
+      });
 
       // ***************************************************************** //
       // DEFINE TASKS
@@ -408,15 +411,15 @@ module.exports = function(grunt) {
         'htmlmin'
     ]);
 
-      // Deploy
-      grunt.registerTask('deploy', [
-        'build',
-        'copy',
-        'buildcontrol'
-      ]);
+    // Deploy
+    grunt.registerTask('deploy', [
+      'build',
+      'copy',
+      'buildcontrol'
+    ]);
 
-      // Default (serve)
-      grunt.registerTask('default', [
-        'serve'
-      ]);
+    // Default (serve)
+    grunt.registerTask('default', [
+      'serve'
+    ]);
 };
---
+++ app/_posts/2015-04-30-scally-blog.markdown
@@ -0,0 +1,11 @@
+---
+layout:             default
+title:              "Welcome to the Scally Blog"
+excerpt:            ""
+exclude_from_nav:   true
+section:            4
+date:               2015-04-30 22:06:25
+categories:         scally
+---
+
+## The Scally Blog
